1718e0d30df83ee9c2dd42ce27c43bc84f29a244
Lasse Lang <lang.lasse@gmx.de>
2022-07-08 09:10:47 +0200

    Added error messages and choosing repos

--- WindowManager.py
+++ WindowManager.py
@@ -1,4 +1,3 @@
-import re
 import PySimpleGUI as sg
 
 backgroundColor = "#323232"
@@ -8,43 +7,92 @@ textColor = "#FFFFFF"
 def mainWindow():
   layout = [
     [
-      sg.Button(
-        "New Repo",
-        button_color=(textColor,elementColor),
-        key="-NEWREPO-",
-        font="Arial 14"
-      ),
-      sg.FolderBrowse(
-        "Open Repo",
-        button_color=(textColor,elementColor),
-        target="-OPENREPO-",
-        font="Arial 14"
-      ),
-      sg.In(
+      sg.Column(
+        [
+          [
+            sg.Button(
+              "New Repo",
+              button_color=(textColor,elementColor),
+              key="-NEWREPO-",
+              font="Arial 14"
+            ),
+            sg.Button(
+              "Clone Repo",
+              button_color=(textColor,elementColor),
+              key="-CLONEREPO-",
+              font="Arial 14"
+            ),
+            sg.FolderBrowse(
+              "Open Repo",
+              button_color=(textColor,elementColor),
+              target="-OPENREPO-",
+              font="Arial 14"
+            ),
+          ],
+          [
+            sg.Listbox(
+              [],
+              key="-REPOFILES-",
+              expand_x=True,
+              expand_y=True,
+              background_color=elementColor,
+              text_color=textColor,
+              sbar_arrow_color=textColor,
+              sbar_background_color=backgroundColor,
+              enable_events=True,
+              font="Arial 14",
+              size=(40, 10)
+            )
+          ]
+        ],
+        background_color=backgroundColor,
         expand_x=True,
-        text_color=textColor,
-        key="-OPENREPO-",
-        change_submits=True,
-        readonly=True,
-        border_width=0,
-        size=(10, 1),
-        font="Arial 18",
-        disabled_readonly_background_color=elementColor
-      )
-    ],
-    [
-      sg.Listbox(
-        [],
-        key="-REPOFILES-",
+        expand_y=True
+      ),
+      sg.Column(
+        [
+          [
+            sg.In(
+              expand_x=True,
+              text_color=textColor,
+              key="-OPENREPO-",
+              change_submits=True,
+              readonly=True,
+              border_width=0,
+              size=(10, 1),
+              font="Arial 18",
+              disabled_readonly_background_color=elementColor
+            )
+          ],
+          [
+            sg.Text(
+              "",
+              expand_x=True,
+              background_color=backgroundColor,
+              text_color=textColor,
+              key="-REPONAME-",
+              font="Arial 20"
+            )
+          ],
+          [
+            sg.Listbox(
+              [],
+              key="-COMMITS-",
+              expand_x=True,
+              expand_y=True,
+              background_color=elementColor,
+              text_color=textColor,
+              sbar_arrow_color=textColor,
+              sbar_background_color=backgroundColor,
+              enable_events=True,
+              font="Arial 14",
+              size=(40, 10)
+            )
+          ]
+        ],
+        background_color=backgroundColor,
         expand_x=True,
-        expand_y=True,
-        background_color=elementColor,
-        text_color=textColor,
-        sbar_arrow_color=textColor,
-        sbar_background_color=backgroundColor,
-        enable_events=True,
-        font="Arial 14",
-        size=(40, 10)
+        expand_y=True
       )
     ]
   ]
@@ -54,4 +102,76 @@ def mainWindow():
     background_color=backgroundColor,
     resizable=True,
     location=(100, 100)
+  )
+
+def errorWindow(message):
+  return sg.PopupError(
+    message,
+    background_color=backgroundColor,
+    text_color=textColor,
+    button_color=(textColor, "red")
+  )
+
+def newRepoWindow():
+  layout = [
+    [
+      sg.Text(
+        "New Repo",
+        font="Arial 22",
+        text_color=textColor,
+        background_color=backgroundColor
+      ),
+    ],
+    [
+      sg.Text(
+        "Name:",
+        font="Arial 14",
+        text_color=textColor,
+        background_color=backgroundColor
+      ),
+      sg.In(
+        font="Arial 14",
+        text_color=textColor,
+        background_color=elementColor,
+        key="-REPONAME-",
+        border_width=0,
+        expand_x=True,
+        size=(20, 1)
+      )
+    ],
+    [
+      sg.FolderBrowse(
+        "Location",
+        button_color=(textColor,elementColor),
+        target="-LOCATION-",
+        font="Arial 14"
+      ),
+      sg.In(
+        expand_x=True,
+        text_color=textColor,
+        key="-LOCATION-",
+        change_submits=True,
+        readonly=True,
+        border_width=0,
+        size=(20, 1),
+        font="Arial 14",
+        disabled_readonly_background_color=elementColor
+      )
+    ],
+    [
+      sg.Button(
+        "Create",
+        button_color=(textColor,elementColor),
+        key="-CREATE-",
+        font="Arial 14",
+        expand_x=True
+      )
+    ]
+  ]
+  return sg.Window(
+    "New Repo",
+    layout,
+    background_color=backgroundColor,
+    location=(100, 100),
+    element_justification="center"
   )
--- main.py
+++ main.py
@@ -6,11 +6,37 @@ import os
 def main():
   mainWindow = WindowManager.mainWindow()
   while True:
-    event, values = mainWindow.read()
+    event, values = mainWindow.read(5000)
     match event:
       case sg.WIN_CLOSED:
         break
+      case "-NEWREPO-":
+        newRepoWindow = WindowManager.newRepoWindow()
+        while True:
+          event, values = newRepoWindow.read()
+          match event:
+            case sg.WIN_CLOSED:
+              break
+            case "-CREATE-":
+              if values["-REPONAME-"] != "" and values["-LOCATION-"] != "":
+                try:
+                  repo = git.Repo.init(f"{values['-LOCATION-']}/{values['-REPONAME-']}")
+                  repo.index.commit('Initial commit.')
+                  mainWindow["-OPENREPO-"].update(f"{values['-LOCATION-']}/{values['-REPONAME-']}")
+                  mainWindow["-REPONAME-"].update(values['-REPONAME-'])
+                  break
+                except:
+                  WindowManager.errorWindow("Couldn't create repo.")
+        newRepoWindow.close()
+      case "-OPENREPO-":
+        try:
+          repo = git.Repo(values["-OPENREPO-"])
+          mainWindow["-REPONAME-"].update(values["-OPENREPO-"].split("/")[-1])
+        except:
+          WindowManager.errorWindow("Can't open repo.")
+          mainWindow["-OPENREPO-"].update("")
   mainWindow.close()
 
 if __name__ == "__main__":
+  global repo
   main()
